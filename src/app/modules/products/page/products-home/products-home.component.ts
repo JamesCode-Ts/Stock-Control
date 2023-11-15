9412cf7605d14f997fcc28ee2db36bcac5d7c644
Tiago <jameslob42@gmail.com>
2023-11-15 19:51:30 -0300

    Implementando tabelas de produtos

---
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { ProductsService } from '../../../../services/products/products.service';
+import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+
+@Component({
+  selector: 'app-products-home',
+  templateUrl: './products-home.component.html',
+  styleUrls: [],
+})
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public productsDatas: Array<GetAllProductsResponse> = [];
+
+  constructor(
+    private productsService: ProductsService,
+    private productsDtService: ProductsDataTransferService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getServiceProductsDatas();
+  }
+
+  getServiceProductsDatas() {
+    const productsLoaded = this.productsDtService.getProductsDatas();
+
+    if (productsLoaded.length > 0) {
+      this.productsDatas = productsLoaded;
+    } else this.getAPIProductsDatas();
+
+  }
+
+  getAPIProductsDatas() {
+    this.productsService
+      .getAllProducts()
+      // o pipe é um médodo utilizado para encadear operadores RxJS, 
+      // È usado para manipular os dados emitidos de um Observable.
+       .pipe(takeUntil(this.destroy$)) // sinaliza que ocorrera uma destruição 
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500,
+          });
+          this.router.navigate(['/dashboard']);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
